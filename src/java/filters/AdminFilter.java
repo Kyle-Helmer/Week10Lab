c4a96be12952c8f6ec5ed564be045b97e9f1f767
Kyle-Helmer <99497831+Kyle-Helmer@users.noreply.github.com>
2022-11-10 08:24:27 -0700

    Lab cloned from Demo Admin Filter added

---
+++ src/java/filters/AdminFilter.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filters;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ *
+ * @author Kyle Helmer
+ */
+public class AdminFilter implements Filter {
+
+   
+
+    public AdminFilter() {
+    }
+
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
+
+
